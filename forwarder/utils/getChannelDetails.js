551c20d4fcb8639819b14f3503f954ecb7d045e0
Apple <apple@Madan.local>
2022-12-04 23:38:25 +0530

    Refactored

--- forwarder/utils/getChannelDetails.js
+++ forwarder/utils/getChannelDetails.js
@@ -1,10 +1,14 @@
 const { Api } = require("telegram");
 
 exports.getChannelDetails = async (channelIds, client) => {
-  const peerSenderDetails = await client.invoke(
-    new Api.channels.GetChannels({
-      id: [...channelIds],
-    })
-  );
-  return peerSenderDetails;
+  try {
+    const peerSenderDetails = await client.invoke(
+      new Api.channels.GetChannels({
+        id: [...channelIds],
+      })
+    );
+    return peerSenderDetails;
+  } catch (err) {
+    throw err;
+  }
 };
