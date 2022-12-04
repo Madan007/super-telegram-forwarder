2007f789f5304e330e7d42b30fbcbae5b2305b7f
Apple <apple@Madan.local>
2022-12-04 22:34:14 +0530

    Provided logic to fetch sender details

--- forwarder/services/processor.js
+++ forwarder/services/processor.js
@@ -3,6 +3,11 @@ const receiver = config.TG_CHANNEL_RECEIVER;
 
 exports.processor = async (event, client) => {
   try {
+    const messageObj = event.message;
+    console.log("Sender id ===", messageObj.senderId);
+    const sender = await messageObj.getSender();
+    console.log("sender Details", sender);
+
     const message = event?.message?.message;
     console.log(" newmessage received >>>>>>>>>", message);
     await client.sendMessage(receiver, { message });
